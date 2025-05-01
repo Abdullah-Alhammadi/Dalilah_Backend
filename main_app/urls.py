0f883f3262db01b3b939a1c76cf84918dadf3a76
Abdullahpc <alhmadi.business@email.com>
2025-05-01 16:30:35 +0300

    start frist models for city

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import Home
+from .views import Home, CityListView
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-
+    path('cities/', CityListView.as_view(), name='city-list'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import City
+from .serializers import CitySerializer
 
 # Define the home view
 class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to Dalilah!'}
     return Response(content)
+  
+
+
+
+
+class CityListView(generics.ListAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
